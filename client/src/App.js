e8999c835b0d0199296c71694271683ce8b7a03a
johnSickels <46609057+johnsickels@users.noreply.github.com>
2019-07-17 21:47:29 -0400

    integrate container

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,21 @@ import React from 'react';
 import FullScreenDialog from "./components/TicTacToe/FullScreenDialog"
 import LogoSweeperDialog from "./components/LogoSweeper/LogoSweeperDialog"
 import RecursiveSnakeDialog from "./components/RecursiveSnake/RecursiveSnakeDialog"
+import Container from "@material-ui/core/Container"
 import './App.css';
 
 function App() {
   return (
     <div>
-       <div className="stars"></div>
-       <div className="twinkling"></div>
-       <div className="clouds"></div>
-       <div></div>
-      <FullScreenDialog />
-      <LogoSweeperDialog />
-      <RecursiveSnakeDialog />
+      <div className="stars"></div>
+      <div className="twinkling"></div>
+      <div className="clouds"></div>
+      <div></div>
+      <Container maxWidth="sm">
+        <FullScreenDialog />
+        <LogoSweeperDialog />
+        <RecursiveSnakeDialog />
+      </Container>
 
     </div>
   );
