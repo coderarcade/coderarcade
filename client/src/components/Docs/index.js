d7608911d18798557b88f26d58c638e98b5b5831
johnSickels <46609057+johnsickels@users.noreply.github.com>
2019-08-03 12:18:50 -0400

    docs and aboutus

---
+++ client/src/components/Docs/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Docs extends React.Component {
+    render() {
+        return (
+            <div style={{ paddingTop: "10px" }} className="container">
+                <div className="card">
+                    <div className="card-body">
+                        <div className="card-title">Global Chat</div>
+                        <hr />
+                        These are the DOCS
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default Docs;
