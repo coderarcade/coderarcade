f9ed03ae27e56ad0f857c0c85bb5d3306a2e0302
johnSickels <46609057+johnsickels@users.noreply.github.com>
2019-08-03 09:11:19 -0400

    env varibale for port

--- client/src/components/Svg/Svg.js
+++ client/src/components/Svg/Svg.js
@@ -1,77 +1,77 @@
 import React from 'react';
 
 function SVG() {
-	return(
+	return (
 
-    <div class="svglogo">
-    <div class="centered"><div class="neon">Coder Aracade</div></div>
+		<div class="svglogo">
+			<div class="centered"><div class="neon">Coder Arcade</div></div>
 
-<svg>
-<defs>
-  
-   <filter id="stroke">
-     <feMorphology operator="dilate" radius="1" 
-in="SourceGraphic" result="outside" />
-     <feMorphology operator="dilate" radius="2" 
-in="outside" result="thickened" />
-     <feComposite operator="out" in2="SourceGraphic" in="thickened" result="stroke"/>
-     
-  </filter>
-  
-  <filter id="inner-glow" >
-    <feFlood flood-color="#e10b8d"/>
-    <feComposite in2="SourceAlpha" operator="out"/>
-    <feGaussianBlur stdDeviation="0.5" result="blur"/>
-    <feComposite operator="atop" in2="SourceGraphic"/>
-    
-</filter>
-  
-  <filter id="outer-glow" >
-  
-		<feMorphology operator="dilate" radius="2" in="SourceAlpha" result="thicken" />
+			<svg>
+				<defs>
 
-	
-		<feGaussianBlur in="thicken" stdDeviation="5" result="blurred" />
+					<filter id="stroke">
+						<feMorphology operator="dilate" radius="1"
+							in="SourceGraphic" result="outside" />
+						<feMorphology operator="dilate" radius="2"
+							in="outside" result="thickened" />
+						<feComposite operator="out" in2="SourceGraphic" in="thickened" result="stroke" />
+
+					</filter>
+
+					<filter id="inner-glow" >
+						<feFlood flood-color="#e10b8d" />
+						<feComposite in2="SourceAlpha" operator="out" />
+						<feGaussianBlur stdDeviation="0.5" result="blur" />
+						<feComposite operator="atop" in2="SourceGraphic" />
+
+					</filter>
+
+					<filter id="outer-glow" >
+
+						<feMorphology operator="dilate" radius="2" in="SourceAlpha" result="thicken" />
 
 
-		<feFlood  flood-color="#db0273" result="glowColor" />
+						<feGaussianBlur in="thicken" stdDeviation="5" result="blurred" />
 
 
-		<feComposite in="glowColor" in2="blurred" operator="in" result="softGlow_colored" />
+						<feFlood flood-color="#db0273" result="glowColor" />
 
-	
-		<feMerge>
-			<feMergeNode in="softGlow_colored"/>
-			<feMergeNode in="SourceGraphic"/>
-		</feMerge>
 
-	</filter>
-  
-  <filter id="outer-glow1" >
-	
-		<feMorphology operator="dilate" radius="20" in="SourceAlpha" result="thicken" />
+						<feComposite in="glowColor" in2="blurred" operator="in" result="softGlow_colored" />
 
-	
-		<feGaussianBlur in="thicken" stdDeviation="25" result="blurred" />
 
-	
-		<feFlood  flood-color="#C0722C" result="glowColor" />
+						<feMerge>
+							<feMergeNode in="softGlow_colored" />
+							<feMergeNode in="SourceGraphic" />
+						</feMerge>
 
-	
-		<feComposite in="glowColor" in2="blurred" operator="in" result="softGlow_colored" />
+					</filter>
 
-		
-		<feMerge>
-			<feMergeNode in="softGlow_colored"/>
-			<feMergeNode in="SourceGraphic"/>
-		</feMerge>
+					<filter id="outer-glow1" >
 
-	</filter>
-  </defs>
-  
-  
-</svg>
-</div>
-)
-  };
+						<feMorphology operator="dilate" radius="20" in="SourceAlpha" result="thicken" />
+
+
+						<feGaussianBlur in="thicken" stdDeviation="25" result="blurred" />
+
+
+						<feFlood flood-color="#C0722C" result="glowColor" />
+
+
+						<feComposite in="glowColor" in2="blurred" operator="in" result="softGlow_colored" />
+
+
+						<feMerge>
+							<feMergeNode in="softGlow_colored" />
+							<feMergeNode in="SourceGraphic" />
+						</feMerge>
+
+					</filter>
+				</defs>
+
+
+			</svg>
+		</div>
+	)
+};
 export default SVG;
