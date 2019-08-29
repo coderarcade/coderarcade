30be1972d9c4542053aa9be5207bb1e7c93a231f
johnSickels <46609057+johnsickels@users.noreply.github.com>
2019-08-29 12:37:54 -0400

    highscores logging

--- client/src/actions/scoreActions.js
+++ client/src/actions/scoreActions.js
@@ -13,9 +13,9 @@ export const sendScore = (userScore) => dispatch => {
     );
 };
 
-export const readScores = (userScore) => dispatch => {
+export const readScores = () => dispatch => {
     axios
-        .get("api/users/scoreboard", userScore)
+        .get("/api/users/scoreboard")
         .then(res => console.log(res))
         .catch(err => console.log(err))
-};
+}
--- client/src/components/Scoreboard/index.js
+++ client/src/components/Scoreboard/index.js
@@ -1,118 +1,140 @@
-import React from "react";
+import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 
 import PropTypes from "prop-types";
 import { readScores } from "../../actions/scoreActions"
 import { connect } from 'react-redux';
 
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+// import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-const useStyles = makeStyles({
-    avatar: {
-        margin: 10,
-    },
-    bigAvatar: {
-        margin: 10,
-        width: 60,
-        height: 60,
-    },
-    paper: {
-        textAlign: 'center',
-        color: "black",
-        background: 'linear-gradient(45deg, #C0722C 30%, #F41B25 90%)',
-        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-        height: "50%",
-        padding: "5%",
-    },
-    table: {
-        minWidth: 650,
-    },
-});
+// import Table from '@material-ui/core/Table';
+// import TableBody from '@material-ui/core/TableBody';
+// import TableCell from '@material-ui/core/TableCell';
+// import TableHead from '@material-ui/core/TableHead';
+// import TableRow from '@material-ui/core/TableRow';
+// import Paper from '@material-ui/core/Paper';
 
-function createData(user, score) {
-    console.log(this);
-    
-    // this.props.readScores();
+// const useStyles = makeStyles({
+//     avatar: {
+//         margin: 10,
+//     },
+//     bigAvatar: {
+//         margin: 10,
+//         width: 60,
+//         height: 60,
+//     },
+//     paper: {
+//         textAlign: 'center',
+//         color: "black",
+//         background: 'linear-gradient(45deg, #C0722C 30%, #F41B25 90%)',
+//         boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+//         height: "50%",
+//         padding: "5%",
+//     },
+//     table: {
+//         minWidth: 650,
+//     },
+// });
 
-    return { user, score };
+// function createData(user, score) {
+//     console.log(this);
+
+//     this.props.readScores();
+
+//     return { user, score };
+// }
+
+// const rows = [
+//     createData('John', 10),
+//     createData('Chelsea', 9),
+//     createData('Calvin', 1),
+// ];
+
+
+
+class Scoreboard extends Component {
+    constructor() {
+        super();
+        this.state = {
+            highScore: []
+        }
+
+    }
+
+    componentWillMount() {
+        // console.log(this.props.readScores);
+        // createData();
+        this.props.readScores()
+
+    }
+
+    render() {
+        return (
+            <Container>
+                High Scores Here!
+            </Container>
+            // <Container style={{ width: "100%", padding: "5%" }} fluid="true">
+            //     <Paper className={classes.paper}><h3 style={{ textAlign: "center" }}>High Scores</h3>
+            //         <Table className={classes.table} size="small">
+            //             <TableHead>
+            //                 <TableRow>
+            //                     <TableCell>Tetris</TableCell>
+            //                     <TableCell align="right">Score</TableCell>
+            //                 </TableRow>
+            //             </TableHead>
+            //             <TableBody>
+            //                 {rows.map(row => (
+            //                     <TableRow key={row.user}>
+            //                         <TableCell component="th" scope="row">
+            //                             {row.user}
+            //                         </TableCell>
+            //                         <TableCell align="right">{row.score}</TableCell>
+            //                     </TableRow>
+            //                 ))}
+            //             </TableBody>
+            //         </Table>
+            //         <Table className={classes.table} size="small">
+            //             <TableHead>
+            //                 <TableRow>
+            //                     <TableCell>Snake</TableCell>
+            //                     <TableCell align="right">Score</TableCell>
+            //                 </TableRow>
+            //             </TableHead>
+            //             <TableBody>
+            //                 {rows.map(row => (
+            //                     <TableRow key={row.user}>
+            //                         <TableCell component="th" scope="row">
+            //                             {row.user}
+            //                         </TableCell>
+            //                         <TableCell align="right">{row.score}</TableCell>
+            //                     </TableRow>
+            //                 ))}
+            //             </TableBody>
+            //         </Table>
+            //         <Table className={classes.table} size="small">
+            //             <TableHead>
+            //                 <TableRow>
+            //                     <TableCell>Logo Sweeper</TableCell>
+            //                     <TableCell align="right">Score</TableCell>
+            //                 </TableRow>
+            //             </TableHead>
+            //             <TableBody>
+            //                 {rows.map(row => (
+            //                     <TableRow key={row.user}>
+            //                         <TableCell component="th" scope="row">
+            //                             {row.user}
+            //                         </TableCell>
+            //                         <TableCell align="right">{row.score}</TableCell>
+            //                     </TableRow>
+            //                 ))}
+            //             </TableBody>
+            //         </Table>
+            //     </Paper>
+            // </Container>
+        );
+    }
 }
-
-const rows = [
-    createData('John', 10),
-    createData('Chelsea', 9),
-    createData('Calvin', 1),
-];
-
-function Scoreboard() {
-    const classes = useStyles();
-
-    return (
-        <div className={classes.root}>
-            <Paper className={classes.paper}><h3 style={{ textAlign: "center" }}>High Scores</h3>
-                <Table className={classes.table} size="small">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Tetris</TableCell>
-                            <TableCell align="right">Score</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map(row => (
-                            <TableRow key={row.user}>
-                                <TableCell component="th" scope="row">
-                                    {row.user}
-                                </TableCell>
-                                <TableCell align="right">{row.score}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-                <Table className={classes.table} size="small">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Snake</TableCell>
-                            <TableCell align="right">Score</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map(row => (
-                            <TableRow key={row.user}>
-                                <TableCell component="th" scope="row">
-                                    {row.user}
-                                </TableCell>
-                                <TableCell align="right">{row.score}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-                <Table className={classes.table} size="small">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Logo Sweeper</TableCell>
-                            <TableCell align="right">Score</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map(row => (
-                            <TableRow key={row.user}>
-                                <TableCell component="th" scope="row">
-                                    {row.user}
-                                </TableCell>
-                                <TableCell align="right">{row.score}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </Paper>
-        </div>
-    );
-} 
 
 Scoreboard.propTypes = {
     readScores: PropTypes.func.isRequired,
